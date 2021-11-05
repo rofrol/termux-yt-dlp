38c89db1de44945d685959d314b9a56bf7aa18eb
Roman Frołow <rofrol@gmail.com>
2021-11-05 15:18:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ TERMUXURLOPENER_CONFIG_FOLDER="${HOME}/bin/"
 
 echo "Hi, This script setup an environment to download various videos from various apps"
 sleep 1
-echo -e "\n\n${BLUE}Requirements :"
-echo -e "${NC}    1. Allow storage access to Termux"
-echo -e "${NC}    2. A working internet connection.\n\n"
+echo -e "\n\n${BLUE}Requirements:${NC}"
+echo -e "    1. Allow storage access to Termux."
+echo -e "    2. A working internet connection.\n\n"
 read -p "When you are ready just press enter:"
 
 # Basic setup
@@ -25,5 +25,4 @@ mkdir -p $TERMUXURLOPENER_CONFIG_FOLDER
 cp -r yt-dlp ~/.config/
 cp termux-url-opener "${TERMUXURLOPENER_CONFIG_FOLDER}/"
 
-echo -e "${BLUE}Congratulations!!! Your setup is complete.\n\n"
-read -p "When you are ready just press enter"
+echo -e "${BLUE}Congratulations!!! Your setup is complete.${NC}\n\n"
